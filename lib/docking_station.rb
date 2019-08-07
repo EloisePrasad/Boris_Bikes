1791d7092290c046b5af992bc291d871042f6525
EloisePrasad <eloise.prasad@outlook.com>
2019-08-07 14:49:08 +0100

    added dock method

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,6 +1,11 @@
-require './bike'
+require './lib/bike'
 class DockingStation
+
   def release_bike
     bike = Bike.new
   end
+
+  def dock(bike)
+  end
+
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -7,4 +7,7 @@ describe DockingStation do
     bike = subject.release_bike
     expect(bike).to be_working
   end
+  it 'dock bike at docking station' do
+    expect(subject).to respond_to(:dock).with(1).argument
+  end
 end
