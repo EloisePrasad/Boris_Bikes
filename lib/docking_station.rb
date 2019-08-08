2cb298427996a3d7614155107f72937af2c90984
EloisePrasad <eloise.prasad@outlook.com>
2019-08-08 16:18:33 +0100

    completed challenge 12,13. Middle of challenge 14, .dock method can only take 20 bikes

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -3,12 +3,18 @@ class DockingStation
 
 attr_reader :bike
 
+  def initialize
+    @bike = []
+  end
+
   def release_bike
-    @bike
+    fail 'No bikes available' if @bike.empty?
+    @bike.pop
   end
 
   def dock(bike)
-    @bike = bike
+    fail 'Dock full' if @bike.count >= 20
+    @bike << bike
   end
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,23 +4,37 @@ describe DockingStation do
   it { is_expected.to respond_to :release_bike }
 
   it 'releases working bikes' do
-    bike = subject.release_bike
-    expect(bike).to be_working
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.release_bike).to be_working
   end
+
   it 'dock bike at docking station' do
     bike = Bike.new
-    expect(subject.dock(bike)).to eq(bike)
+    expect(subject.dock(bike)).to eq([bike])
   end
+
   it 'can return the bike that docked' do
     bike = Bike.new
     subject.dock(bike)
-    expect(subject.bike).to eq(bike)
+    expect(subject.bike).to eq([bike])
   end
+
   describe '#release_bike' do
     it 'releases a bike' do
       bike = Bike.new
       subject.dock(bike)
       expect(subject.release_bike).to eq bike
+    end
+    it 'raises an error when there are no bikes available' do
+      expect { subject.release_bike }.to raise_error 'No bikes available'
+    end
+  end
+
+  describe '#dock' do
+    it 'raises error when full' do
+      20.times { subject.dock(Bike.new) }
+      expect { subject.dock(Bike.new) }.to raise_error 'Dock full'
     end
   end
 end
